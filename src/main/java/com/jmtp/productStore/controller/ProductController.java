bb4d89ca9b276ef93e58786514f22d9bef861710
juancho <jm.ticona.p@gmail.com>
2022-08-30 23:55:52 -0500

    [FIX] correct URL for image

--- src/main/java/com/jmtp/productStore/controller/ProductController.java
+++ src/main/java/com/jmtp/productStore/controller/ProductController.java
@@ -18,6 +18,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -49,13 +50,22 @@ public class ProductController {
     }
 
     @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
-    public List<Product> getAll(){
-        return productService.getAll();
+    public List<ProductResponse<Map<String, Object>>> getAll(HttpServletRequest request){
+        List<ProductResponse<Map<String, Object>>> result = new ArrayList<>();
+        List<Product> list_products = productService.getAll();
+        list_products.stream()
+                .forEach(prod -> {
+                    result.add( makeProductResponsable(prod, request) );
+                });
+        return result;
     }
 
     @GetMapping(value = "/{productId}", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public Product getProduct(@PathVariable String productId){
-        return productService.getProduct(productId);
+    public ProductResponse<Map<String, Object>> getProduct(@PathVariable String productId, HttpServletRequest request){
+        //Por uso de Mongo Atlas se debe crear el URI de la imagen a la salida
+        Product product = productService.getProduct(productId);
+
+        return makeProductResponsable(product, request);
     }
 
     @GetMapping(value = "/{productId}/image", produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE})
@@ -83,6 +93,39 @@ public class ProductController {
         return new ProductResponse<Boolean>(
                 productService.purchase(cart)
         );
+    }
+
+    /***
+     *
+     * @param product
+     * @param request
+     * @return :the full URI to get the image resorce
+     */
+    private String makeImagePath( Product product, HttpServletRequest request ){
+        //https://jmtpproductstore.herokuapp.com/api/v1/product/630ecd17ffe5ad336fae0d9c/image
+        return String.format(
+                "%s://%s/api/v1/product/%s/image",
+                request.getScheme(),
+                request.getHeader("host"),
+                product.getId() );
+    }
+
+    /***
+     *
+     * @param product
+     * @param request
+     * @return
+     */
+    private ProductResponse<Map<String,Object>> makeProductResponsable(Product product, HttpServletRequest request){
+        Map<String, Object> result = new HashMap<>();
+        result.put("id", product.getId());
+        result.put("name", product.getName());
+        result.put("price", product.getPrice());
+        result.put("imagePath", makeImagePath(product, request));
+        result.put("stock", product.getStock());
+        result.put("category", product.getCategory());
+
+        return new ProductResponse<Map<String,Object>>( result );
     }
 
 }
